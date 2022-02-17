ddf6a9a7d300fe10533c61726e099343322cda2e
Sonne <sonnne34@gmil.com>
2022-02-17 18:04:45 +0500

    add GameViewModel

---
+++ app/src/main/java/com/sonne/computation/presentation/GameViewModel.kt
@@ -0,0 +1,104 @@
+package com.sonne.computation.presentation
+
+import android.os.CountDownTimer
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.sonne.computation.domian.entity.GameSettings
+import com.sonne.computation.domian.entity.Level
+import com.sonne.computation.domian.entity.Question
+import com.sonne.computation.domian.repository.GameRepositoryImpl
+import com.sonne.computation.domian.usecases.GenerateQuestionUseCase
+import com.sonne.computation.domian.usecases.GetGameSettingsUseCase
+
+
+class GameViewModel : ViewModel() {
+
+    private lateinit var level: Level
+    private lateinit var gameSettings: GameSettings
+
+    private val repository = GameRepositoryImpl
+
+    private val generateQuestionUseCase = GenerateQuestionUseCase(repository)
+    private val getGameSettingsUseCase = GetGameSettingsUseCase(repository)
+
+    private var timer: CountDownTimer? = null
+
+    private val _formattedTime = MutableLiveData<String>()
+    val formattedTime: LiveData<String>
+        get() = _formattedTime
+
+    private val _question = MutableLiveData<Question>()
+    val question: LiveData<Question>
+        get() = _question
+
+    private var countOfRightAnswers = 0
+    private var countOfQuestions = 0
+
+    fun startGame(level: Level) {
+        getGameSettings(level)
+        startTimer()
+        generateQuestion()
+    }
+
+    fun chooseAnswer(number: Int) {
+        checkAnswer(number)
+        generateQuestion()
+    }
+
+    private fun checkAnswer(number: Int) {
+        val rightAnswer = question.value?.rightAnswer
+        if (number == rightAnswer) {
+            countOfRightAnswers++
+        }
+        countOfQuestions++
+    }
+
+    private fun getGameSettings(level: Level) {
+        this.level = level
+        this.gameSettings = getGameSettingsUseCase.invoke(level)
+    }
+
+    private fun startTimer() {
+        timer = object : CountDownTimer(
+            gameSettings.gameTimeInSeconds * MILLIS_IN_SECOND,
+            MILLIS_IN_SECOND
+        ) {
+            override fun onTick(millisUntilFinished: Long) {
+                _formattedTime.value = formatTime(millisUntilFinished)
+            }
+
+            override fun onFinish() {
+                finishGame()
+            }
+        }
+        timer?.start()
+    }
+
+    private fun generateQuestion() {
+        _question.value = generateQuestionUseCase(gameSettings.maxSumValue)
+    }
+
+    private fun finishGame() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        timer?.cancel()
+    }
+
+    private fun formatTime(millisUntilFinished: Long): String {
+        val seconds = millisUntilFinished / MILLIS_IN_SECOND
+        val minutes = seconds / SECONDS_IN_MINUTES
+        val leftSeconds = seconds - (minutes * SECONDS_IN_MINUTES)
+        return String.format("%02d:%02d", minutes, leftSeconds)
+    }
+
+    companion object {
+
+        const val MILLIS_IN_SECOND = 1000L
+        const val SECONDS_IN_MINUTES = 60
+    }
+
+}
